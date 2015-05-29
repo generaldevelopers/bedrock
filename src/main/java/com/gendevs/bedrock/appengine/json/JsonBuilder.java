d71bb0433d0fcc63e93523061ca1dc0eee935085
Deepak Bandela <deepak@gendevs.com>
2015-05-29 14:59:20 +0530

    Json filter changes

--- src/main/java/com/gendevs/bedrock/appengine/json/JsonBuilder.java
+++ src/main/java/com/gendevs/bedrock/appengine/json/JsonBuilder.java
@@ -18,8 +18,6 @@ package com.gendevs.bedrock.appengine.json;
 
 
 public interface JsonBuilder {
-
 	public <T> T getEntityForJson(String json, Class<T> entity);
-
 	public String getJsonForEntity(JsonInterface entity);
 }
