5807e031975a11c43923ae4112701345e3a4a928
Deepak Bandela <deepak@gendevs.com>
2015-06-12 15:02:11 +0530

    Server response is changed to suit the actual response object

--- src/main/java/com/gendevs/bedrock/appengine/response/ServerResponse.java
+++ src/main/java/com/gendevs/bedrock/appengine/response/ServerResponse.java
@@ -19,22 +19,26 @@ package com.gendevs.bedrock.appengine.response;
 import com.gendevs.bedrock.appengine.json.JsonInterface;
 
 public class ServerResponse<T> implements JsonInterface<T> {
+
 	public String message;
 	public int statusCode;
 	public boolean successful;
 	public T data;
+
 	public ServerResponse(boolean successful, String message, int statusCode, T data) {
 		this.successful = successful;
 		this.message = message;
 		this.statusCode = statusCode;
 		this.data = data;
 	}
+
 	public ServerResponse(T data){
 		this.successful = true;
 		this.message = "successful";
 		this.statusCode = 200;
 		this.data = data;
 	}
+
 	public ServerResponse() {
 		
 	}
