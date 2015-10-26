1b7100aacfdc6cc9a87884b207d4422054a23d7e
deepakbandela <deepak.bandela@gmail.com>
2015-10-26 12:31:07 +0530

    Organizing the code in PagingAndSortingRepository

--- src/main/java/com/gendevs/bedrock/appengine/repository/PagingAndSortingRepository.java
+++ src/main/java/com/gendevs/bedrock/appengine/repository/PagingAndSortingRepository.java
@@ -23,9 +23,7 @@ import org.springframework.data.domain.Sort;
 import java.io.Serializable;
 
 public interface PagingAndSortingRepository<T, ID extends Serializable> extends CrudRepository<T, ID> {
-
 	Iterable<T> findAll(Sort sort);
-
 	Page<T> findAll(Pageable pageable);
 }
 
