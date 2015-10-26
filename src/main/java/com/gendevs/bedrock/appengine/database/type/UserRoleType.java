74fe8bba1a327f0a6c4d87aa17b76e107b92a904
deepakbandela <deepak.bandela@gmail.com>
2015-10-26 12:28:09 +0530

    Changed roles to reflect all roles

--- src/main/java/com/gendevs/bedrock/appengine/database/type/UserRoleType.java
+++ src/main/java/com/gendevs/bedrock/appengine/database/type/UserRoleType.java
@@ -20,7 +20,5 @@ public class UserRoleType {
 	public static final String SUPER_ADMIN = "SUPER_ADMIN";
 	public static final String ADMIN = "ADMIN";
 	public static final String USER = "USER";
-	
 	public static final String GUEST = "GUEST";
-
 }
